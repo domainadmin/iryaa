f26028b478c5437ca184ad9e4eb0006118d58372
Anumira Technologies <administrator@anumira.com>
2013-01-12 20:02:28 +0530

    iryaa

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iryaa::Application.config.secret_token = '33195da44fcdb4419c1cd27ff4a28ba5c66eda5fdc7452a237040a5124ab98ad7eb2239c7eb36ae061028fc292b1b4797a5308cf4f6581a080d435a2700f1c96'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Iryaa::Application.routes.draw do
+  get "home/index"
+
+  root :to => 'home#index'
+end
